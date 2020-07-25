bf312a4c216cff59cd74c4a1eba6f9fa9d4c46a4
aisolab <bsk0130@gmail.com>
2020-07-25 17:38:09 +0900

    feat: model subpackage 구현  - net.py  - ops.py
     Refs: #4

--- src/model/net.py
+++ src/model/net.py
@@ -1,4 +1,30 @@
-"""
-    This script was made by Nick at 19/07/20.
-    To implement code of your network using operation from ops.py.
-"""
+import torch
+import torch.nn as nn
+from .ops import MultiChannelEmbedding, ConvolutionLayer, MaxOverTimePooling
+from ..utils.vocab import Vocab
+
+
+class SenCNN(nn.Module):
+    """SenCNN class"""
+
+    def __init__(self, num_classes: int, vocab: Vocab) -> None:
+        """Instantiating SenCNN class
+
+        Args:
+            num_classes (int): the number of classes
+            vocab (Vocab): the instance of Vocab
+        """
+        super(SenCNN, self).__init__()
+        self._embedding = MultiChannelEmbedding(vocab)
+        self._convolution = ConvolutionLayer(self._embedding._static.size()[1], 300)
+        self._pooling = MaxOverTimePooling()
+        self._dropout = nn.Dropout()
+        self._fc = nn.Linear(300, num_classes)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        fmap = self._embedding(x)
+        fmap = self._convolution(fmap)
+        feature = self._pooling(fmap)
+        feature = self._dropout(feature)
+        score = self._fc(feature)
+        return score
--- src/model/ops.py
+++ src/model/ops.py
@@ -1,16 +1,81 @@
-"""
-    This script was made by Nick at 19/07/20.
-    To implement code of your operation to be being used your network.
-"""
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+from ..utils.vocab import Vocab
+from typing import Tuple
 
 
-def multiply(a, b):
-    return a * b
+class MultiChannelEmbedding(nn.Module):
+    """MultiChannelEmbedding class"""
+
+    def __init__(self, vocab: Vocab) -> None:
+        """Instantiating MultiChannelEmbedding class
+
+        Args:
+            vocab (Vocab): the instance of Vocab
+        """
+        super(MultiChannelEmbedding, self).__init__()
+        self._static = nn.Embedding.from_pretrained(
+            torch.from_numpy(vocab.embedding),
+            freeze=True,
+            padding_idx=vocab.to_indices(vocab.padding_token),
+        )
+        self._non_static = nn.Embedding.from_pretrained(
+            torch.from_numpy(vocab.embedding),
+            freeze=False,
+            padding_idx=vocab.to_indices(vocab.padding_token),
+        )
+        
+    def forward(self, x: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
+        static = self._static(x).permute(0, 2, 1)
+        non_static = self._non_static(x).permute(0, 2, 1)
+        return static, non_static
 
 
-def add(a, b):
-    return a + b
+class ConvolutionLayer(nn.Module):
+    """ConvolutionLayer class"""
+
+    def __init__(self, in_channels: int, out_channels: int) -> None:
+        """Instantiating ConvolutionLayer class
+
+        Args:
+            in_channels (int): the number of channels from input feature map
+            out_channels (int): the number of channels from output feature map
+        """
+        super(ConvolutionLayer, self).__init__()
+        self._tri_gram_conv = nn.Conv1d(
+            in_channels=in_channels, out_channels=out_channels // 3, kernel_size=3
+        )
+        self._tetra_gram_conv = nn.Conv1d(
+            in_channels=in_channels, out_channels=out_channels // 3, kernel_size=4
+        )
+        self._penta_gram_conv = nn.Conv1d(
+            in_channels=in_channels, out_channels=out_channels // 3, kernel_size=5
+        )
+
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor]) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+        static, non_static = x
+        tri_feature_map = F.relu(self._tri_gram_conv(static)) + F.relu(self._tri_gram_conv(non_static))
+        tetra_feature_map = F.relu(self._tetra_gram_conv(static)) + F.relu(
+            self._tetra_gram_conv(non_static)
+        )
+        penta_feature_map = F.relu(self._penta_gram_conv(static)) + F.relu(
+            self._penta_gram_conv(non_static)
+        )
+        return tri_feature_map, tetra_feature_map, penta_feature_map
 
 
-def subtract(a, b):
-    return a - b
+class MaxOverTimePooling(nn.Module):
+    """MaxOverTimePooling class"""
+
+    def forward(self, x: Tuple[torch.Tensor, torch.Tensor, torch.Tensor]) -> torch.Tensor:
+        tri_feature_map, tetra_feature_map, penta_feature_map = x
+        fmap = torch.cat(
+            [
+                tri_feature_map.max(dim=-1)[0],
+                tetra_feature_map.max(dim=-1)[0],
+                penta_feature_map.max(dim=-1)[0],
+            ],
+            dim=-1,
+        )
+        return fmap
