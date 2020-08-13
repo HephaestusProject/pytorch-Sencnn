bddf3e08cf6dcbcd24802c33f5adedfa286367fa
aisolab <bsk0130@gmail.com>
2020-08-14 01:07:24 +0900

    feat: SST2Corpus 등록 및 오타수정
     Ref: #15

--- src/utils/corpus.py
+++ src/utils/corpus.py
@@ -30,4 +30,28 @@ class NSMCCorpus(Dataset):
         return tokens2indices, label
 
 
-CorpusRegistry = {"nsmc": NSMCCorpus}
+class SST2Corpus(Dataset):
+    """SST2Corpus class"""
+
+    def __init__(self, filepath: str, encode_fn: Callable[[str], List[int]]) -> None:
+        """Instantiating SST2Corpus class
+
+        Args:
+
+            filepath (str): filepath
+            encode_fn (Callable): a function that can act as a encoder
+        """
+        self._corpus = pd.read_csv(filepath, sep="\t").loc[:, ["document", "label"]]
+        self._encode_fn = encode_fn
+
+    def __len__(self) -> int:
+        return len(self._corpus)
+
+    def __getitem__(self, idx: int) -> Tuple[torch.Tensor, torch.Tensor]:
+        tokens2indices = torch.tensor(
+            self._encode_fn(self._corpus.iloc[idx]["document"])
+        )
+        label = torch.tensor(self._corpus.iloc[idx]["label"])
+        return tokens2indices, label
+
+CorpusRegistry = {"nsmc": NSMCCorpus, "sst2": SST2Corpus}
