e2dfbea3cb2133ac77b9215c1fc1ac0e866d42ad
aisolab <bsk0130@gmail.com>
2020-08-26 00:20:35 +0900

    refactor: conf 구조에 따른 build_dataset.py 수정
     Refs: #19

--- build_dataset.py
+++ build_dataset.py
@@ -26,71 +26,56 @@ def create_dataset(filepath: Path) -> pd.DataFrame:
 
 def main(args: Namespace) -> None:
     config_dir = Path("conf")
-    dataset_config_dir = config_dir / "dataset"
-    parent_dir = Path("dataset")
-    child_dir = parent_dir / args.dataset
-    dataset_config_path = dataset_config_dir / f"{args.dataset}.yaml"
-    dataset_config = OmegaConf.load(dataset_config_path)
+    dataset_dir = Path("dataset")
+    task_config_dir = config_dir / args.task
+    task_pipeline_config_dir = task_config_dir / "pipeline"
+    task_dataset_dir = dataset_dir / args.task
 
-    if args.dataset == "nsmc":
+    task_pipeline_config_path = task_pipeline_config_dir / f"{args.pipeline}.yaml"
+    task_pipeline_config = OmegaConf.load(task_pipeline_config_path)
+
+    if args.task == "nsmc":
         # loading dataset
-        dataset = pd.read_csv(dataset_config.path.train, sep="\t").loc[
+        dataset = pd.read_csv(task_pipeline_config.dataset.path.train, sep="\t").loc[
             :, ["document", "label"]
         ]
         dataset = dataset.loc[dataset["document"].isna().apply(lambda elm: not elm), :]
         train, validation = train_test_split(
             dataset, test_size=args.valid_ratio, random_state=args.seed
         )
-        test = pd.read_csv(dataset_config.path.test, sep="\t").loc[:, ["document", "label"]]
+        test = pd.read_csv(task_pipeline_config.dataset.path.test, sep="\t").loc[:, ["document", "label"]]
         test = test.loc[test["document"].isna().apply(lambda elm: not elm), :]
 
-        path_dict = {
-            "train": str(child_dir / "train.txt"),
-            "validation": str(child_dir / "validation.txt"),
-            "test": str(child_dir / "test.txt")
-        }
+    elif args.task == "trec6":
 
-        dataset_config.path.update(path_dict)
-        OmegaConf.save(dataset_config, dataset_config_path)
-
-        train.to_csv(dataset_config.path.train, sep="\t", index=False)
-        validation.to_csv(dataset_config.path.validation, sep="\t", index=False)
-        test.to_csv(dataset_config.path.test, sep="\t", index=False)
-
-    elif args.dataset == "trec6":
-
-        config_dir = Path("conf")
-        dataset_config_dir = config_dir / "dataset"
-        parent_dir = Path("dataset")
-        child_dir = parent_dir / "trec6"
-        dataset_config_path = dataset_config_dir / "trec6.yaml"
-        dataset_config = OmegaConf.load(dataset_config_path)
-        dataset = create_dataset(dataset_config.path.train)
+        dataset = create_dataset(task_pipeline_config.dataset.path.train)
         dataset = dataset.loc[dataset["document"].isna().apply(lambda elm: not elm), :]
 
         train, validation = train_test_split(
             dataset, test_size=args.valid_ratio, random_state=args.seed
         )
 
-        test = create_dataset(dataset_config.path.test)
+        test = create_dataset(task_pipeline_config.dataset.path.test)
         test = test.loc[dataset["document"].isna().apply(lambda elm: not elm), :]
 
-        path_dict = {
-            "train": str(child_dir / "train.txt"),
-            "validation": str(child_dir / "validation.txt"),
-            "test": str(child_dir / "test.txt")
-        }
-        dataset_config.path.update(path_dict)
-        OmegaConf.save(dataset_config, dataset_config_path)
+    path_dict = {
+        "train": str(task_dataset_dir / "train.txt"),
+        "validation": str(task_dataset_dir / "validation.txt"),
+        "test": str(task_dataset_dir / "test.txt")
+    }
 
-        train.to_csv(dataset_config.path.train, sep="\t", index=False)
-        validation.to_csv(dataset_config.path.validation, sep="\t", index=False)
-        test.to_csv(dataset_config.path.test, sep="\t", index=False)
+    task_pipeline_config.dataset.path.update(path_dict)
+    OmegaConf.save(task_pipeline_config, task_pipeline_config_path)
+
+    train.to_csv(task_pipeline_config.dataset.path.train, sep="\t", index=False)
+    validation.to_csv(task_pipeline_config.dataset.path.validation, sep="\t", index=False)
+    test.to_csv(task_pipeline_config.dataset.path.test, sep="\t", index=False)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--dataset", type=str, default="nsmc", choices=["nsmc", "trec6"])
+    parser.add_argument("--task", type=str, default="nsmc", choices=["nsmc", "trec6"])
+    parser.add_argument("--pipeline", type=str, default="pv00")
     parser.add_argument("--valid_ratio", type=float, default=0.1)
     parser.add_argument("--seed", type=int, default=42)
     args = parser.parse_args()
